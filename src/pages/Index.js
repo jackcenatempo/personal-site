dc9ab058103405585a861d79ba61a28b31fee513
Jack Cenatempo <v-jcenatempo@microsoft.com>
2023-08-26 14:32:48 -0400

    image tweaks

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -14,9 +14,8 @@ const Index = () => (
         <Link to="/resume">resume / CV. </Link>
       </p>
     </article>
-    <article className="post" id="index"><p>I&apos;m not sure what to put here, so here are a few charts I&apos;ve made over the past few months.
-       They don&apos;t mean anything together, so don&apos;t read into it too much!</p>
-      <Link to="/" className="logo"><img src={`${PUBLIC_URL}/images/sample_of_work_8_23.JPG`} alt="" />
+    <article className="post" id="index"><p>I&apos;m not sure what to put here, so here are a few charts I&apos;ve made over the past few months. They don&apos;t mean anything together, so don&apos;t read into them too much!</p>
+      <Link to="/" className="logo"><img src={`${PUBLIC_URL}/images/sample_of_work_8_23.png`} alt="" />
       </Link>
     </article>
   </Main>
