00013f1788c4bf0c7040254a14ca809687bcdaf2
Jack Cenatempo <98119404+jackcenatempo@users.noreply.github.com>
2025-01-21 18:43:54 -0500

    adding links to 'not found' page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -9,7 +9,7 @@ const PageNotFound = () => (
         <meta name="description" content="The content you are looking for cannot be found." />
       </Helmet>
       <h1>Page Not Found</h1>
-      <h3>If you&apos;re looking for my email address, please refer to my CV.</h3>
+      <h3>Looking to contact me? Please refer to <Link to="https://docs.google.com/document/d/1vTDXQ_l5cApoQfFeKJOR1U5WDdqzQVagThDioREzfhA/edit?usp=sharing">my CV</Link> or add me on <Link to="https://www.linkedin.com/in/jackcenatempo/">LinkedIn</Link>.</h3>
       <p>Return <Link to="/">home</Link>.</p>
     </div>
   </HelmetProvider>
